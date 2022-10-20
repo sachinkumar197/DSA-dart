5132e3d48dbc3f90a940954f75bea5fc74ff9771
sachin <sachin.kumar@vayana.com>
2022-10-21 02:05:04 +0530

    array rotation with temp array

--- arrayRotation/arrayRotation.dart
+++ arrayRotation/arrayRotation.dart
@@ -1,1 +1,16 @@
- 
+// https://www.geeksforgeeks.org/array-rotation/
+// Problem : Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+
+// Using Temp array
+
+List<dynamic> rotateArray(List<int> _list, int d) {
+  var tempList = [];
+  tempList = _list.sublist(d);
+  tempList += _list.sublist(0, d);
+  return tempList;
+}
+
+void main() {
+  var list = [1, 2, 3, 4, 5, 6, 7];
+  return print(rotateArray(list, 2));
+}
